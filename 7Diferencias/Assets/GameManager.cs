58f3bbb967fa0bcd460484b2be663b4a233da632
Juan Francisco Guillén Navarro <jfran191297@gmail.com>
2020-04-03 14:05:55 +0200

    Creación de mecánicas
    Se han hecho las mecánicas del juego y además se ha hecho en todas las láminas

---
+++ 7Diferencias/Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] diferencias;
+
+    public int numDiferencias;
+    public Text diferenciasRestantes;
+
+    public Canvas canvasDiferencias;
+    public Canvas canvasVictoria;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diferencias = GameObject.FindGameObjectsWithTag("Diferencia");
+        numDiferencias = diferencias.Length;
+
+        canvasDiferencias.enabled = true;
+        canvasVictoria.enabled = false;
+    }
+
+    private void Update()
+    {
+        diferenciasRestantes.text = numDiferencias.ToString();
+
+        if(numDiferencias <= 0)
+        {
+            canvasDiferencias.enabled = false;
+            canvasVictoria.enabled = true;
+        }
+    }
+}
---
+++ 7Diferencias/Assets/Scripts/ScriptDiferencia.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDiferencia : MonoBehaviour
+{
+
+    public SpriteRenderer thisRenderer;
+
+    public bool encontrada;
+
+    public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRenderer = GetComponent<SpriteRenderer>();
+        thisRenderer.enabled = false;
+        encontrada = false;
+
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    private void OnMouseDown()
+    {
+        thisRenderer.enabled = true;
+        encontrada = true;
+
+        gameManager.GetComponent<GameManager>().numDiferencias--;
+        var colliders = this.GetComponents<Collider2D>();
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].enabled = false;
+        }
+    }
+}
